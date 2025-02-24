febe04788f465240ec422afa6922ea32d03bc3c6
Karen Corona Espinoza <110923327+KarenCorona@users.noreply.github.com>
2025-02-24 09:12:37 -0600

    Update specialcharacter.spec.js

--- tests/specialcharacter.spec.js
+++ tests/specialcharacter.spec.js
@@ -64,7 +64,7 @@ afterEach(async function () {
   it('special character', async function() {
     await driver.get("http://127.0.0.1:8000/"); // Cambia la URL si es necesario
     await driver.manage().window().setRect({ width: 654, height: 656 });
-    await driver.findElement(By.id("num1")).sendKeys("1");
+    await driver.findElement(By.id("num1")).sendKeys("2");
     await driver.findElement(By.id("num2")).sendKeys("2");
     await driver.findElement(By.css("button:nth-child(1)")).click();
 
