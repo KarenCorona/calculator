aef01eaec97af795215d9ec197f8e97a43b90340
Karen Corona Espinoza <110923327+KarenCorona@users.noreply.github.com>
2025-03-03 08:49:16 -0600

    Update subnumbers.spec.js

--- tests/subnumbers.spec.js
+++ tests/subnumbers.spec.js
@@ -71,7 +71,7 @@ afterEach(async function () {
 });
 
   it('Numbers', async function() {
-    await driver.get("http://127.0.0.1:5500/src/src/")
+    await driver.get("http://127.0.0.1:8000/src/src/")
     await driver.manage().window().setRect({ width: 654, height: 656 })
     await driver.findElement(By.id("num1")).sendKeys("3")
     await driver.findElement(By.id("num2")).click()
