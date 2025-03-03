4994853c362be4e2114802f683b2e8b45a856ed8
Karen Corona Espinoza <110923327+KarenCorona@users.noreply.github.com>
2025-03-03 08:48:54 -0600

    Update subpositiveandnegative.spec.js

--- tests/subpositiveandnegative.spec.js
+++ tests/subpositiveandnegative.spec.js
@@ -71,7 +71,7 @@ afterEach(async function () {
 });
 
   it('positive and negative', async function() {
-    await driver.get("http://127.0.0.1:5500/src/")
+    await driver.get("http://127.0.0.1:8000/src/")
     await driver.manage().window().setRect({ width: 654, height: 656 })
     await driver.findElement(By.id("num1")).sendKeys("5")
     await driver.findElement(By.id("num2")).click()
